369dd8492e5d531399151532cd1c98933a961b0b
Cinquin Andy <andy.cinquin@kleegroup.com>
2021-10-01 11:26:48 +0200

    add somes routes, and fixs somes bugs, full dynamic routes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const {request, response} = require("express");
 const app = express();
 const port = process.env.PORT;
 const db = require('./queries')
@@ -13,14 +12,19 @@ app.use(
     })
 )
 
-app.get('/', function (req, res) {
-    res.send('hello world')
-})
-
 app.get('/tables',db.getTables);
+app.get('/line/:table',db.getLineFromTable);
 app.get(/^\/klee_.*/,db.dynamics);
-app.post(/^\/klee_.*/, db.dynamicsBeetweenDate);
+app.post(/^\/klee_.*/, db.dynamicsBetweenDate);
+app.get(/^\/average\/klee_.*/,db.dynamicsAverage);
+app.post(/^\/average\/klee_.*/, db.dynamicsAverageBetweenDate);
 
 app.listen(port, () => {
-    console.log(`App Running on port ${port}`);
+    console.log(`--> App Running on port ${port}`);
+    console.log(`You can call now : http://localhost:${port}/tables to get all table's name`);
+    console.log(`You can call now : http://localhost:${port}//line/:table to get all line's name from a table`);
+    console.log(`You can call now : http://localhost:${port}/klee_<.*> to get all data from table's name`);
+    console.log(`You can call now [with post 2 dates] : http://localhost:${port}/klee_<.*> to get all data from table's name between 2 dates`);
+    console.log(`You can call now : http://localhost:${port}/average/klee_<.*> to get average data from table's name`);
+    console.log(`You can call now [with post 2 dates] : http://localhost:${port}/average/klee_<.*> to get average data from table's name between 2 dates`);
 });
--- queries.js
+++ queries.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
-const string = require("string-sanitizer");
-const {Pool, query} = require('pg')
-const {response, request} = require("express");
-const regexTable = /^klee(.*)/;
-var listTables = [];
+require("string-sanitizer");
+const {Pool} = require('pg');
 
-function sanitizeString(str){
-    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+// from w3c official post
+function sanitizeString(str) {
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
     return str.trim();
 }
-function sanitizeDate(str){
-    str = str.replace(/[^a-z0-9áéíóúñü \.,:_-]/gim,"");
+
+// from w3c official post [ modify to don't sanitize ":" ]
+function sanitizeDate(str) {
+    str = str.replace(/[^a-z0-9áéíóúñü \.,:_-]/gim, "");
     return str.trim();
 }
 
@@ -24,49 +24,178 @@ const pool = new Pool(
     }
 );
 
-const getTables = (request, response) => {
-    pool.query('SELECT * FROM information_schema.tables;', (error, result) => {
+// get all tables name, from a regex sql query
+const getTables = (req, res) => {
+    pool.query('SELECT table_name FROM information_schema.tables where table_name ~ \'^klee(.*)\';', (error, result) => {
         if (error) {
             throw error
         }
-        var table = [];
-        for (const row of result.rows) {
-            if (row.table_name.match(/^klee(.*)/)) {
-                table.push(row.table_name);
-            }
-        }
-        response.status(200).json(table);
+        res.status(200).json(result.rows);
     })
 }
 
+// get all lines from a table name, regex sql query too
+const getLineFromTable = (req, res) => {
+    const params = sanitizeString(req.originalUrl.replace(/^\/line\//, ""));
+    if (params.match(/^klee(.*)/)) {
+        pool.query(`select *
+                    from (select column_name
+                          from information_schema.columns
+                          where table_schema = 'public'
+                            and table_name = '${params}') as result
+                    where column_name != 'time'
+                      and column_name != 'unite';`, (error, result) => {
+            if (error) {
+                throw error
+            }
+            res.status(200).json(result.rows);
+        })
+    } else {
+        res.status(200).json({error: "aucune tables n'existe avec ce nom"})
+    }
+}
 
+// get all values from a table
 const dynamics = (req, res) => {
     const params = sanitizeString(req.originalUrl.substring(1));
-    pool.query(
-        `SELECT * FROM ${params};`,
-        (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).json(results.rows);
-    })
+    if (params.match(/^klee(.*)/)) {
+        pool.query(`SELECT *
+                    FROM ${params};`, (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.status(200).json(results.rows);
+        })
+    } else {
+        res.status(200).json({error: "aucune tables n'existe avec ce nom"})
+    }
 }
 
-const dynamicsBeetweenDate = (req, res) => {
-    const params = sanitizeString(req.originalUrl.substring(1).replace(/\/.*$/,""));
-    let { date_start, date_end } = req.query
-    date_start=sanitizeDate(date_start)
-    date_end=sanitizeDate(date_end)
-    pool.query(`SELECT * FROM public.${params} where time between '${date_start}' and '${date_end}';`, (error, results) => {
-        if (error) {
-            throw error
+// get all values from a table between 2 dates
+const dynamicsBetweenDate = (req, res) => {
+    const params = sanitizeString(req.originalUrl.substring(1).replace(/\/.*$/, ""));
+    if (params.match(/^klee(.*)/)) {
+        let {date_start, date_end} = req.query
+        date_start = sanitizeDate(date_start)
+        date_end = sanitizeDate(date_end)
+        if (date_start > date_end) {
+            // if the starting date is higher than the end, then swap the values
+            let date_temp = date_start
+            date_start = date_end
+            date_end = date_temp
         }
-        res.status(201).json(results.rows);
-    })
+        pool.query(`SELECT *
+                    FROM public.${params}
+                    where time between '${date_start}' and '${date_end}';`, (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.status(201).json(results.rows);
+        })
+    } else {
+        res.status(200).json({error: "aucune tables n'existe avec ce nom"})
+    }
+}
+
+// get average values from all line of a table -> parameter : only name of the table
+const dynamicsAverage = (req, res) => {
+    // match string started by /average/ , and delete it
+    let params = req.originalUrl.replace(/^\/average\//, "");
+    // match string started by "klee" and finished by / or end of the line
+    params = sanitizeString(params.replace(/\/.*$/, "").match(/^klee(.*)/)[0]);
+    // if params started by "klee" the url was valid, then we can give a response
+    if (params.match(/^klee(.*)/)) {
+        let arrayColumn = [];
+        pool.query(`select *
+                    from (select column_name
+                          from information_schema.columns
+                          where table_schema = 'public'
+                            and table_name = '${params}') as result
+                    where column_name != 'time'
+                      and column_name != 'unite';`, (error, results) => {
+            if (error) {
+                throw error
+            }
+            for (const row of results.rows) {
+                for (const rowKey in row) {
+                    arrayColumn.push(row[rowKey])
+                }
+            }
+            let queryPrep = "";
+            for (const arrayColumnKey in arrayColumn) {
+                queryPrep += ((arrayColumnKey == 0) ? "" : ",") +
+                    `AVG(${arrayColumn[arrayColumnKey]}) as "${arrayColumn[arrayColumnKey]}"`;
+            }
+            pool.query(`select ${queryPrep}
+                        from ${params};`, (errorSecondQuery, resultsSecondQuery) => {
+                if (errorSecondQuery) {
+                    throw errorSecondQuery
+                }
+                res.status(200).json(resultsSecondQuery.rows);
+            })
+        })
+    } else {
+        res.status(200).json({error: "aucune tables n'existe avec ce nom"})
+    }
+}
+
+// get average values from all line of a table, between 2 dates
+const dynamicsAverageBetweenDate = (req, res) => {
+    // match string started by /average/ , and delete it
+    let params = req.originalUrl.replace(/^\/average\//, "");
+    // match string started by "klee" and finished by / or end of the line
+    params = sanitizeString(params.replace(/\/.*$/, "").match(/^klee(.*)/)[0]);
+    // if params started by "klee" the url was valid, then we can give a response
+    if (params.match(/^klee(.*)/)) {
+        let {date_start, date_end} = req.query
+        date_start = sanitizeDate(date_start)
+        date_end = sanitizeDate(date_end)
+        if (date_start > date_end) {
+            // if the starting date is higher than the end, then swap the values
+            let date_temp = date_start
+            date_start = date_end
+            date_end = date_temp
+        }
+        let arrayColumn = [];
+        pool.query(`select *
+                    from (select column_name
+                          from information_schema.columns
+                          where table_schema = 'public'
+                            and table_name = '${params}') as result
+                    where column_name != 'time'
+                      and column_name != 'unite';`, (error, results) => {
+            if (error) {
+                throw error
+            }
+            for (const row of results.rows) {
+                for (const rowKey in row) {
+                    arrayColumn.push(row[rowKey])
+                }
+            }
+            let queryPrep = "";
+            for (const arrayColumnKey in arrayColumn) {
+                queryPrep += ((arrayColumnKey == 0) ? "" : ",") +
+                    `AVG(${arrayColumn[arrayColumnKey]}) as "${arrayColumn[arrayColumnKey]}"`;
+            }
+            pool.query(`select ${queryPrep}
+                        from ${params}
+                        where time between '${date_start}' and '${date_end}';`, (errorSecondQuery, resultsSecondQuery) => {
+                if (errorSecondQuery) {
+                    throw errorSecondQuery
+                }
+                res.status(201).json(resultsSecondQuery.rows);
+            })
+        })
+    } else {
+        res.status(200).json({error: "aucune tables n'existe avec ce nom"})
+    }
 }
 
 module.exports = {
     dynamics,
-    dynamicsBeetweenDate,
+    dynamicsBetweenDate,
+    dynamicsAverage,
+    dynamicsAverageBetweenDate,
+    getLineFromTable,
     getTables
 }
